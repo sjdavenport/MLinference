51a73911accdcc58c6c9622287af5ac4ef6c7443
Samuel Davenport <sdavenport@Samuels-MacBook-Pro.local>
2024-10-01 15:33:30 +0200

    backup

--- DeepLearning/Polyps/saveimages/save_orig_ims.m
+++ DeepLearning/Polyps/saveimages/save_orig_ims.m
@@ -2,11 +2,11 @@ path4ims = '/Users/sdavenport/Documents/Data/SegmentationData/polyps/examples/';
 
 im_size = [530,600];
 
-saveloc = '/Users/sdavenport/Documents/MyPapers/0Papers/2024_crsegmentation/figures/val_images/';
+saveloc = '/Users/sdavenport/Documents/MyPapers/0Papers/2024_crsegmentation/figures/all_images/';
 
-for I = 1:275
+for I = 1:500
     I
-    ex = val_ex_indices(I);
+    ex = ex_indices(I);
 
     true_mask = imread([path4ims, num2str(ex),'_gt_mask.jpg']);
     im = imread([path4ims, num2str(ex),'.jpg']);
