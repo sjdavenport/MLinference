f2a8dfe4239bf3267acf187654509644fefeaff0
Samuel Davenport <sdavenport@Samuels-MacBook-Pro.local>
2024-10-01 20:29:39 +0200

    backup

--- DeepLearning/Polyps/saveimages/save_orig_ims.m
+++ DeepLearning/Polyps/saveimages/save_orig_ims.m
@@ -74,3 +74,31 @@ for I = 1:59
     % clf
     % close all
 end
+
+%% Learning dataset masks
+learning_idx_ex = learning_idx - 1;
+learn_ex_indices = intersect(ex_indices, learning_idx_ex);
+
+path4ims = '/Users/sdavenport/Documents/Data/SegmentationData/polyps/examples/';
+
+im_size = [530,600];
+
+saveloc = '/Users/sdavenport/Documents/MyPapers/0Papers/2024_crsegmentation/figures/learning/truemasks/';
+% cmap = custom_colormap([1,0.5,0.5], [1,1,0]);
+% cmap = custom_colormap([0.8,0.4,0.8], [1,1,0]);
+
+% for I = 1:length(learn_ex_indices)
+for I = 55:59
+    I
+    ex = learn_ex_indices(I);
+    score_im = predicted_outer_boxes(:,:,ex+1);
+    score_im_re = imresize(score_im, im_size, 'nearest');
+
+    imagesc(score_im > 0.5)
+    % % colormap(cmap)
+    axis off image
+    screenshape([1,1,799,705], 'white')
+    saveim([num2str(ex), '.png'], saveloc)
+    % clf
+    % close all
+end
